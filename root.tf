51399f68cdf7a1613f769149d6ab3b91d88c3c42
Ben Cordero <bencord0@condi.me>
2019-05-13 01:01:06 +0100

    Add geopoiesis application resources
    https://docs.geopoiesis.io/manual/app-setup/aws-setup

---
+++ root.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {}
+
+module "geopoiesis-backend" {
+  source = "github.com/geopoiesis/terraform//aws?ref=0.6.0"
+  region = "eu-west-2"
+}
+
+module "geopoiesis-user" {
+  source = "github.com/geopoiesis/terraform//aws/iam_user?ref=0.6.0"
+
+  policy_arn = "${module.geopoiesis-backend.policy_arn}"
+}
