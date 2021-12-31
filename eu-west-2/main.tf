736ea918b06395abd2f66cca9ab079d6a1f1d5df
Ben Cordero <bencord0@condi.me>
2021-12-31 10:59:57 +0000

    Move `identites` to `modules/identites`
    And refactor the outputs to be a bit more generic

--- eu-west-2/main.tf
+++ eu-west-2/main.tf
@@ -1,7 +1,7 @@
 resource "random_uuid" "uuid" {}
 
 module "identities" {
-  source = "../identities"
+  source = "../modules/identities"
 
   oidc_client_id     = local.slack_oidc_client_id
   oidc_client_secret = local.slack_oidc_client_secret
@@ -11,11 +11,12 @@ resource "aws_iam_openid_connect_provider" "main" {
   url = module.identities.cognito_url
 
   client_id_list = [
-    module.identities.cognito_client_id,
+    module.identities.user_pool_client_id,
   ]
 
   thumbprint_list = [
-    # this is a guess, based on a test account
+    # This is the thumbprint for cognito-idp.eu-west-2.amazonaws.com
+    # and is not sufficient to uniquely identify our own user pools
     "9e99a48a9960b14926bb7f3b02e22da2b0ab7280",
   ]
 }
--- modules/identities/outputs.tf
+++ modules/identities/outputs.tf
@@ -2,10 +2,18 @@ output "cognito_url" {
   value = "https://cognito-idp.eu-west-2.amazonaws.com/${aws_cognito_user_pool.main.id}"
 }
 
-output "cognito_client_id" {
+output "user_pool_arn" {
+  value = aws_cognito_user_pool.main.arn
+}
+
+output "user_pool_endpoint" {
+  value = aws_cognito_user_pool.main.endpoint
+}
+
+output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.main.id
 }
 
-output "cognito_client_secret" {
+output "user_pool_client_secret" {
   value = aws_cognito_user_pool_client.main.client_secret
 }
