054ec59cf1a977d2729dc13b7c9ebcbade5c9d82
Ben Cordero <bencord0@condi.me>
2022-11-26 15:58:32 +0000

    Provision mastodon containers on ECS Fargate

---
+++ eu-west-1/ecs.tf
@@ -0,0 +1,186 @@
+resource "aws_ecs_cluster" "default" {
+  name = "default"
+
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
+}
+
+resource "aws_ecs_service" "mastodon" {
+  name = "mastodon"
+  cluster = aws_ecs_cluster.default.id
+
+  platform_version = "LATEST"
+
+  # AWS Managed, when on Fargate
+  # service role: Used to register into load balancers
+  iam_role    = "aws-service-role"
+
+  # task role: Used by the containers, referenced in task definition
+  depends_on      = [aws_iam_role.mastodon-task-role]
+  task_definition = aws_ecs_task_definition.mastodon.arn
+
+  propagate_tags                    = "NONE"
+  health_check_grace_period_seconds = 60
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.mastodon-web.arn
+    container_name   = "web"
+    container_port   = 3000
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.mastodon-streaming.arn
+    container_name   = "streaming"
+    container_port   = 4000
+  }
+
+  network_configuration {
+    subnets          = module.private_subnets.subnet_ids
+    security_groups  = [aws_security_group.default.id]
+    assign_public_ip = false
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    base              = 1
+    weight            = 1
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    base              = 0
+    weight            = 1
+  }
+
+  desired_count = 1
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
+
+resource "aws_ecs_task_definition" "mastodon" {
+  family = "mastodon"
+
+  cpu    = "512"
+  memory = "1024"
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  execution_role_arn = aws_iam_role.mastodon-execution-role.arn
+  task_role_arn      = aws_iam_role.mastodon-task-role.arn
+
+  container_definitions = jsonencode([
+    {
+      name         = "web"
+      image        = "${aws_ecr_repository.mastodon.repository_url}:${local.container_image_tag}"
+      command      = ["bundle", "exec", "puma", "-C", "config/puma.rb"]
+      environment  = [for k, v in local.environment : {"name": k, "value": v}]
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-create-group"  = "true"
+          "awslogs-group"         = "/ecs/mastodon"
+          "awslogs-region"        = "eu-west-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+        secretOptions = []
+      }
+    },
+
+    {
+      name         = "streaming"
+      image        = "${aws_ecr_repository.mastodon.repository_url}:${local.container_image_tag}"
+      command      = ["node", "./streaming"]
+      environment  = [for k, v in local.environment : {"name": k, "value": v}]
+      essential    = true
+      portMappings = [
+        {
+          containerPort = 4000
+          hostPort      = 4000
+          protocol      = "tcp"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-create-group"  = "true"
+          "awslogs-group"         = "/ecs/mastodon"
+          "awslogs-region"        = "eu-west-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+        secretOptions = []
+      }
+    },
+
+    {
+      name        = "sidekiq"
+      image       = "${aws_ecr_repository.mastodon.repository_url}:${local.container_image_tag}"
+      command     = ["bundle", "exec", "sidekiq"]
+      environment = [for k, v in local.environment : {"name": k, "value": v}]
+      essential   = true
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-create-group"  = "true"
+          "awslogs-group"         = "/ecs/mastodon"
+          "awslogs-region"        = "eu-west-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+        secretOptions = []
+      }
+    }
+  ])
+
+  depends_on = [aws_iam_role.mastodon-task-role]
+}
+
+locals {
+  container_image_tag = "4.0.2"
+
+  environment = {
+    # Mastodon (via kt-paperclip) does not know how to use task role credentials
+    AWS_ACCESS_KEY_ID     = aws_iam_access_key.mastodon-useruploads.id
+    AWS_SECRET_ACCESS_KEY = aws_iam_access_key.mastodon-useruploads.secret
+
+    DATABASE_URL           = data.consul_keys.mastodon.var.database_url
+    REDIS_URL              = data.consul_keys.mastodon.var.redis_url
+
+    BIND                   = "0.0.0.0"
+    LOCAL_DOMAIN           = "nfra.club"
+    RAILS_ENV              = "production"
+    STREAMING_API_BASE_URL = "wss://streaming.nfra.club"
+
+    OTP_SECRET             = data.consul_keys.mastodon.var.otp_secret
+    SECRET_KEY_BASE        = data.consul_keys.mastodon.var.secret_key_base
+
+    S3_BUCKET              = aws_s3_bucket.mastodon-useruploads.id
+    S3_ENABLED             = "true"
+    S3_REGION              = "eu-west-1"
+    S3_ALIAS_HOST          = "useruploads.nfra.club"
+
+    VAPID_PRIVATE_KEY = data.consul_keys.mastodon.var.vapid_private_key
+    VAPID_PUBLIC_KEY  = data.consul_keys.mastodon.var.vapid_public_key
+
+  }
+}
--- eu-west-1/variables.tf
+++ eu-west-1/variables.tf
@@ -1,3 +1,35 @@
 variable "region" {
   default = "eu-west-1"
 }
+
+data "consul_keys" "mastodon" {
+  key {
+    name = "database_url"
+    path = "condime/terraform_state/mastodon/database_url"
+  }
+
+  key {
+    name = "redis_url"
+    path = "condime/terraform_state/mastodon/redis_url"
+  }
+
+  key {
+    name = "otp_secret"
+    path = "condime/terraform_state/mastodon/otp_secret"
+  }
+
+  key {
+    name = "secret_key_base"
+    path = "condime/terraform_state/mastodon/secret_key_base"
+  }
+
+  key {
+    name = "vapid_private_key"
+    path = "condime/terraform_state/mastodon/vapid_private_key"
+  }
+
+  key {
+    name = "vapid_public_key"
+    path = "condime/terraform_state/mastodon/vapid_public_key"
+  }
+}
