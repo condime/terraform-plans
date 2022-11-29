efd252e3c1c04c2b0b1bf6561d70df34f9be7313
Ben Cordero <bencord0@condi.me>
2022-11-29 00:08:58 +0000

    Fix load balancer attachments
    The order of objects given to `merge()` is important, with
    later objects overwriting keys in earlier objects.
    
    In our case, we were removing the load balancer port mappings.
    - swap around the object merge order in container definitions

--- eu-west-1/containers.tf
+++ eu-west-1/containers.tf
@@ -1,28 +1,9 @@
 locals {
   web_container_definitions = [
-    {
-      name        = "web"
-      command     = ["bundle", "exec", "puma", "-C", "config/puma.rb"]
-      cpu         = 0
-      environment = [for k, v in local.environment : { "name" : k, "value" : v }]
-      essential   = true
-      image       = "${aws_ecr_repository.mastodon.repository_url}${local.container_image_tag}"
+    merge(local.common_container_definitions, {
+      name    = "web"
+      command = ["bundle", "exec", "puma", "-C", "config/puma.rb"]
 
-      linuxParameters = {
-        initProcessEnabled = true
-      }
-
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-create-group"  = "true"
-          "awslogs-group"         = "/ecs/mastodon"
-          "awslogs-region"        = "eu-west-1"
-          "awslogs-stream-prefix" = "ecs"
-        }
-        secretOptions = []
-      }
-      mountPoints = []
       portMappings = [
         {
           containerPort = 3000
@@ -30,27 +11,11 @@ locals {
           protocol      = "tcp"
         }
       ]
-      volumesFrom = []
-    },
+    }),
 
-    {
-      name        = "streaming"
-      command     = ["node", "./streaming"]
-      cpu         = 0
-      environment = [for k, v in local.environment : { "name" : k, "value" : v }]
-      essential   = true
-      image       = "${aws_ecr_repository.mastodon.repository_url}${local.container_image_tag}"
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-create-group"  = "true"
-          "awslogs-group"         = "/ecs/mastodon"
-          "awslogs-region"        = "eu-west-1"
-          "awslogs-stream-prefix" = "ecs"
-        }
-        secretOptions = []
-      }
-      mountPoints = []
+    merge(local.common_container_definitions, {
+      name    = "streaming"
+      command = ["node", "./streaming"]
       portMappings = [
         {
           containerPort = 4000
@@ -58,15 +23,14 @@ locals {
           protocol      = "tcp"
         }
       ]
-      volumesFrom = []
-    },
+    }),
   ]
 
   sidekiq_container_definitions = [
-    {
+    merge(local.common_container_definitions, {
       # Everything except for "scheduler"
-      name        = "sidekiq"
-      command     = [
+      name = "sidekiq"
+      command = [
         "bundle", "exec", "sidekiq",
         "-q", "default",
         "-q", "ingress",
@@ -74,55 +38,43 @@ locals {
         "-q", "pull",
         "-q", "push",
       ]
-      cpu         = 0
-      environment = [for k, v in local.environment : { "name" : k, "value" : v }]
-      essential   = true
-      image       = "${aws_ecr_repository.mastodon.repository_url}${local.container_image_tag}"
-
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-create-group"  = "true"
-          "awslogs-group"         = "/ecs/mastodon"
-          "awslogs-region"        = "eu-west-1"
-          "awslogs-stream-prefix" = "ecs"
-        }
-        secretOptions = []
-      }
-
-      mountPoints  = []
-      portMappings = []
-      volumesFrom  = []
-    },
+    }),
   ]
 
   # Only ever run one of these
   sidekiq_scheduler_container_definitions = [
-    {
-      name        = "sidekiq-scheduler"
-      command     = [
+    merge(local.common_container_definitions, {
+      name = "sidekiq-scheduler"
+      command = [
         "bundle", "exec", "sidekiq", "-q", "scheduler",
       ]
-      cpu         = 0
-      environment = [for k, v in local.environment : { "name" : k, "value" : v }]
-      essential   = true
-      image       = "${aws_ecr_repository.mastodon.repository_url}${local.container_image_tag}"
-
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-create-group"  = "true"
-          "awslogs-group"         = "/ecs/mastodon"
-          "awslogs-region"        = "eu-west-1"
-          "awslogs-stream-prefix" = "ecs"
-        }
-        secretOptions = []
-      }
-
-      mountPoints  = []
-      portMappings = []
-      volumesFrom  = []
-    },
+    }),
   ]
+
+  common_container_definitions = {
+    cpu         = 0
+    environment = [for k, v in local.environment : { "name" : k, "value" : v }]
+    essential   = true
+    image       = "${aws_ecr_repository.mastodon.repository_url}${local.container_image_tag}"
+
+    linuxParameters = {
+      initProcessEnabled = true
+    }
+
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        "awslogs-create-group"  = "true"
+        "awslogs-group"         = "/ecs/mastodon"
+        "awslogs-region"        = "eu-west-1"
+        "awslogs-stream-prefix" = "ecs"
+      }
+      secretOptions = []
+    }
+
+    mountPoints  = []
+    portMappings = []
+    volumesFrom  = []
+  }
 }
 
