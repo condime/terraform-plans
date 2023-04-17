81e810e92b46b680a635143448c1893f2d312800
Ben Cordero <bencord0@condi.me>
2023-04-17 01:56:49 +0100

    Allow shelling into alternative workers

--- scripts/ecs_exec.sh
+++ scripts/ecs_exec.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-TASK_ARN="$(aws ecs list-tasks --service-name mastodon-sidekiq | jq -r '.taskArns[0]')"
-exec aws ecs execute-command --cluster default --task "${TASK_ARN}" --container sidekiq --interactive --command /bin/bash
+SERVICE_NAME="${1:-mastodon-web}"
+declare -A CONTAINER_NAMES=(
+    [mastodon-web]=web
+    [mastodon-sidekiq]=sidekiq
+)
+CONTAINER_NAME="${CONTAINER_NAMES[${SERVICE_NAME}]}"
+
+TASK_ARN="$(aws ecs list-tasks --service-name "${SERVICE_NAME}" | jq -r '.taskArns[0]')"
+exec aws ecs execute-command --cluster default --task "${TASK_ARN}" --container "${CONTAINER_NAME}" --interactive --command /bin/bash
