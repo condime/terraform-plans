9e8174036f37f92c067b96c80ebbb2dec4b5f4f5
Ben Cordero <bencord0@condi.me>
2023-10-29 20:44:20 +0000

    Debug AWS Credentials

---
+++ scripts/debug_aws_credentials.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "AWS - Get Caller Identity"
+aws sts get-caller-identity
+
+echo
+echo "AWS ROLE DATA"
+echo "AWS_ROLE_ARN = ${AWS_ROLE_ARN}"
+echo "AWS_WEB_IDENTITY_TOKEN_FILE = ${AWS_WEB_IDENTITY_TOKEN_FILE}"
+ls -l "${AWS_WEB_IDENTITY_TOKEN_FILE}"
+echo
+echo "GITHUB_ENV = ${GITHUB_ENV}"
