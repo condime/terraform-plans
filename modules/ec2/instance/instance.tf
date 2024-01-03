9ee11bf7707f75b01eb0f34f792da2dac21b225c
Ben Cordero <bencord0@condi.me>
2024-01-03 20:14:09 +0000

    Bump EC2 base image values

--- modules/ec2/instance/instance.tf
+++ modules/ec2/instance/instance.tf
@@ -69,11 +69,11 @@ resource "aws_launch_template" "this" {
 data "aws_ami" "al2023" {
   owners      = ["amazon"]
   most_recent = true
-  name_regex  = "^al2023-ami-2023.1."
+  name_regex  = "^al2023-ami-2023.2."
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.1.*"]
+    values = ["al2023-ami-2023.2.*"]
   }
 
   filter {
